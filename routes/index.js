d9b7f61f14216b0522368c73d5b91af61e83d28b
Dylan Helpling <dkhelpling@gmail.com>
2019-03-25 17:32:45 -0500

    Deleted conditional from routes and added middleware

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,56 @@
 var express = require("express");
 var router = express.Router();
 var User = require("../models/User");
+var mid = require("../middleware");
 
 // Get the homepage
 router.get("/", function(req, res, next) {
-  if (req.session) {
-    return res.render("index_admin", { title: "Home" });
-    console.log("test works for admin");
-  } else {
-    return res.render("index", { title: "Home" });
-    console.log("test works");
-  }
+  return res.render("index", { title: "Home" });
+  console.log("test works");
 });
 
 // Get the food pages
 router.get("/food", function(req, res, next) {
-  if (req.session) {
-    return res.render("food_admin", { title: "Food" });
-    console.log("test works for admin");
-  } else {
-    return res.render("food", { title: "Food" });
-    console.log("test works");
-  }
+  return res.render("food", { title: "Food" });
+  console.log("test works");
 });
 
 // Get travel page
 router.get("/travel", function(req, res, next) {
-  if (req.session) {
-    return res.render("travel_admin", { title: "Travel" });
-    console.log("test works for admin");
-  } else {
-    return res.render("travel", { title: "Travel" });
-    console.log("test works");
-  }
+  return res.render("travel", { title: "Travel" });
+  console.log("test works");
 });
 
 // Get movies page
-router.get("/movies", function(req, res, next) {
-  if (req.session) {
-    return res.render("movie_admin", { title: "Movie" });
-    console.log("test works for admin");
-  } else {
-    return res.render("movie", { title: "Movie" });
-    console.log("test works");
-  }
+router.get("/movie", function(req, res, next) {
+  return res.render("movie", { title: "Movie" });
+  console.log("test works");
 });
 
 // Get gryffin page
 router.get("/gryffin", function(req, res, next) {
-  if (req.session) {
-    return res.render("gryffin_admin", { title: "Gryffin" });
-    console.log("test works for admin");
-  } else {
-    return res.render("gryffin", { title: "Gryffin" });
-    console.log("test works");
-  }
+  return res.render("gryffin", { title: "Gryffin" });
+  console.log("test works");
+});
+
+router.get("/gryffin_admin", mid.requiresLogin, function(req, res, next) {
+  return res.render("gryffin", { title: "Gryffin" });
+  console.log("test works");
+});
+
+router.get("/food_admin", mid.requiresLogin, function(req, res, next) {
+  return res.render("food", { title: "Food" });
+  console.log("test works");
+});
+
+router.get("/travel_admin", mid.requiresLogin, function(req, res, next) {
+  return res.render("travel", { title: "Travel" });
+  console.log("test works");
+});
+
+router.get("/movie_admin", mid.requiresLogin, function(req, res, next) {
+  return res.render("movie", { title: "Movie" });
+  console.log("test works");
 });
 
 /* GET admin login */
@@ -62,7 +58,6 @@ router.get("/admin", function(req, res, next) {
   return res.render("admin", { title: "Admin" });
   console.log("test works for admin");
 });
-module.exports = router;
 
 // Post /admin
 router.post("/admin", function(req, res, next) {
@@ -74,7 +69,7 @@ router.post("/admin", function(req, res, next) {
         return next(err);
       } else {
         req.session.userId = user._id;
-        return res.redirect("/index");
+        return res.redirect("/");
       }
     });
   } else {
@@ -83,4 +78,40 @@ router.post("/admin", function(req, res, next) {
     return next(err);
   }
 });
+
+router.get("/register", function(req, res, next) {
+  return res.render("register", { title: "Sign up" });
+});
+
+router.post("/register", function(req, res, next) {
+  if (req.body.email && req.body.password && req.body.confirmPassword) {
+    // Confirm that user typed same password twice
+    if (req.body.password !== req.body.confirmPassword) {
+      var err = new Error("Passwords do not match .");
+      err.status = 400;
+      return next(err);
+    }
+
+    // create object with form input
+    var userData = {
+      email: req.body.email,
+      password: req.body.password
+    };
+
+    // use schema's  'create' method to insert document into Mongo
+    User.create(userData, function(error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect("/index_admin");
+      }
+    });
+  } else {
+    var err = new Error("All fields required.");
+    err.status = 400;
+    return next(err);
+  }
+});
+
 module.exports = router;
